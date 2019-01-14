512f3ca82207e90ba04c85cf085021ffbd2dc3d0
Vanessa Frenkel <vanefrenkel@gmail.com>
2019-01-14 19:15:20 -0300

    tercer commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#one'
   get 'pages/two'
+  get 'pages/three'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
